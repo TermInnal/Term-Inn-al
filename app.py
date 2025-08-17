e6f59ff01f6aefeb3e2cc6d43a2657b2c75e56b9
TermInnal <terminnal8@gmail.com>
2025-08-17 15:49:01 -0300

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-import streamlit as st
-import subprocess
 
-st.set_page_config(page_title="Term-Inn-al", layout="centered")
-st.title("Term-Inn-al")
-
-try:
-    result = subprocess.run(["python", "main.py"], capture_output=True, text=True)
-    st.text(result.stdout)
-except Exception as e:
-    st.error(f"Erro ao executar o jogo: {e}")
